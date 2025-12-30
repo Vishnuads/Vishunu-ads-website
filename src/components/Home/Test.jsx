c141fe15a280fe497f11a04fe50f253ed1b9ddb5
Ezhil-Dev <johnnydepp1853@gmail.com>
2025-12-30 10:14:46 +0530

    test-pg/vh

--- src/components/Home/Test.jsx
+++ src/components/Home/Test.jsx
@@ -36,7 +36,7 @@ const Test = () => {
 
     return (
         <>
-            <section ref={ref} className='test  mb-20 relative  min-h-[260vh] md:min-h-[220vh] lg:min-h-[230vh] '>
+            <section ref={ref} className='test  mb-20 relative  min-h-[260vh] md:min-h-[250vh]  '>
                 <div className='absolute inset-0  bg-linear-to-b from-black/90 via-black/10 to-black/10'></div>
                 <div className=' text-white  absolute inset-0 '>
                     <div className="text-center  py-10">
